c6e90350197a6d38822931ec18195b4d8a11de3c
Paul Xi <paulxi@gmail.com>
2018-04-02 20:19:10 -0700

    Improve LongestSubstringWithoutRepeatingCharacters Solution

--- LeetCodeSwift/Algorithm/Medium/LongestSubstringWithoutRepeatingCharactersSolution.swift
+++ LeetCodeSwift/Algorithm/Medium/LongestSubstringWithoutRepeatingCharactersSolution.swift
@@ -6,23 +6,48 @@
 import Foundation
 
 class LongestSubstringWithoutRepeatingCharactersSolution {
+    // comment because it causes Time Limit Exceeded in leetcode when it handles very long string
+//    func lengthOfLongestSubstring(_ str: String) -> Int {
+//        let strLen = str.count
+//        var maxLength = 0
+//        var startIndex = 0
+//        var currentIndex = 0
+//        var map: [String: Int] = [:]
+//
+//        while maxLength + startIndex < strLen {
+//            let range = str.index(str.startIndex, offsetBy: currentIndex) ..<
+//                    str.index(str.startIndex, offsetBy: currentIndex + 1)
+//            let char = str.substring(with: range)
+//
+//            let pos = map[char] ?? -1
+//            startIndex = max(startIndex, pos + 1)
+//            maxLength = max(maxLength, currentIndex - startIndex + 1)
+//            map[char] = currentIndex
+//            currentIndex += 1
+//        }
+//
+//        return maxLength
+//    }
+
     func lengthOfLongestSubstring(_ str: String) -> Int {
+        var iterator = str.makeIterator()
         let strLen = str.count
         var maxLength = 0
         var startIndex = 0
         var currentIndex = 0
-        var map: [String: Int] = [:]
+        var map: [Character: Int] = [:]
 
-        while maxLength + startIndex < strLen {
-            let range = str.index(str.startIndex, offsetBy: currentIndex) ..<
-                    str.index(str.startIndex, offsetBy: currentIndex + 1)
-            let char = str.substring(with: range)
+        while let char = iterator.next() {
+            if ( maxLength + startIndex < strLen) {
+                let pos = map[char] ?? -1
+                startIndex = max(startIndex, pos + 1)
+                maxLength = max(maxLength, currentIndex - startIndex + 1)
+                map[char] = currentIndex
+                currentIndex += 1
+            } else {
+                break
+            }
 
-            let pos = map[char] ?? -1
-            startIndex = max(startIndex, pos + 1)
-            maxLength = max(maxLength, currentIndex - startIndex + 1)
-            map[char] = currentIndex
-            currentIndex += 1
         }
 
         return maxLength
