a703787470347a1b425c6558a7d58bb27a9ad91b
Paul Xi <paulxi@gmail.com>
2018-10-13 19:37:55 -0700

    Submit Merge Sorted Array Solution

---
+++ LeetCodeSwift/Algorithm/Easy/MergeSortedArraySolution.swift
@@ -0,0 +1,24 @@
+//
+// Created by Xi, Paul on 10/13/18.
+// Copyright (c) 2018 LeetCode. All rights reserved.
+//
+
+import Foundation
+
+class MergeSortedArraySolution {
+    func merge(_ nums1: inout [Int], _ mLen: Int, _ nums2: [Int], _ nLen: Int) {
+        var iIndex = mLen - 1;
+        var jIndex = nLen - 1;
+        var kIndex = mLen + nLen - 1;
+        while jIndex >= 0 {
+            if iIndex < 0 || nums2[jIndex] > nums1[iIndex] {
+                nums1[kIndex] = nums2[jIndex]
+                jIndex -= 1
+            } else {
+                nums1[kIndex] = nums1[iIndex]
+                iIndex -= 1
+            }
+            kIndex -= 1
+        }
+    }
+}
---
+++ LeetCodeSwiftTests/Algorithm/Easy/MergeSortedArraySolutionSpec.swift
@@ -0,0 +1,22 @@
+//
+// Created by Xi, Paul on 10/13/18.
+// Copyright (c) 2018 LeetCode. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import LeetCodeSwift
+
+class MergeSortedArraySolutionSpec: QuickSpec {
+    override func spec() {
+        describe("Merge Sorted Array") {
+            it("Test case 1") {
+                let solution = MergeSortedArraySolution()
+                var nums1 = [1, 2, 3, 0, 0, 0]
+                let nums2 = [2, 5, 6]
+                solution.merge(&nums1, 3, nums2, 3)
+                expect(nums1).to(equal([1, 2, 2, 3, 5, 6]))
+            }
+        }
+    }
+}
