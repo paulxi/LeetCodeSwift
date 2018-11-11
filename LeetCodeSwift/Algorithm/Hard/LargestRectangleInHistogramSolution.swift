005f84291b583a4a87e07dc51511489703c60f0d
Paul Xi <paulxi@gmail.com>
2018-11-10 17:39:57 -0800

    Improve Largest Rectangle in Histogram Solution

--- LeetCodeSwift/Algorithm/Hard/LargestRectangleInHistogramSolution.swift
+++ LeetCodeSwift/Algorithm/Hard/LargestRectangleInHistogramSolution.swift
@@ -7,32 +7,39 @@ import Foundation
 
 class LargestRectangleInHistogramSolution {
     func largestRectangleArea(_ heights: [Int]) -> Int {
-        return calculateArea(heights, 0, heights.count - 1)
-    }
-
-    private func calculateArea(_ heights: [Int], _ start: Int, _ end: Int) -> Int {
-        guard start <= end else {
+        if heights.count == 0 {
             return 0
         }
 
-        var shortest = heights[start]
-        var shortestIndex = start
-
-        if start == end {
-            return shortest
-        } else {
-            for idx in (start + 1) ... end {
-                if heights[idx] < shortest {
-                    shortest = heights[idx]
-                    shortestIndex = idx
-                }
-            }
-
-            let area = shortest * (end - start + 1)
-            let leftArea = calculateArea(heights, start, shortestIndex - 1)
-            let rightArea = calculateArea(heights, shortestIndex + 1, end)
-
-            return max(area, leftArea, rightArea)
+        if heights.count == 1 {
+            return heights[0]
         }
+
+        var lessFromLeft:[Int] = Array(repeating: 0, count: heights.count)
+        var lessFromRight:[Int] = Array(repeating: 0, count: heights.count)
+        lessFromLeft[0] = -1
+        lessFromRight[heights.count - 1] = heights.count
+
+        for idx in 1 ..< heights.count {
+            var ptr = idx - 1
+            while ptr >= 0 && heights[ptr] >= heights[idx] {
+                ptr = lessFromLeft[ptr]
+            }
+            lessFromLeft[idx] = ptr
+        }
+
+        for idx in stride(from: heights.count - 2, through: 0, by: -1) {
+            var ptr = idx + 1
+            while ptr < heights.count && heights[ptr] >= heights[idx] {
+                ptr = lessFromRight[ptr]
+            }
+            lessFromRight[idx] = ptr
+        }
+
+        var maxArea = 0
+        for idx in 0 ..< heights.count {
+            maxArea = max(maxArea, heights[idx] * (lessFromRight[idx] - lessFromLeft[idx] - 1))
+        }
+        return maxArea;
     }
 }
